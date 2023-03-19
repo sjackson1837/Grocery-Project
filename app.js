49d5f97bfba7833d50107729ec0c2e0c74ea116a
sjackson1837 <sjackson330@gmail.com>
2023-03-19 18:57:25 -0500

    init

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ function getProductInfo() {
 		$('#product-name').text(product.product_name);
 		$('#product-description').text(product.generic_name);
 		$('#product-image').attr('src', product.image_url);
+		var audio = new Audio('Sounds/positive.mp3');
+		audio.play();
 	  } else {
+		var audio = new Audio('Sounds/negative.mp3');
+		audio.play();
 		$('#product-name').text('no product found');
 	  }
 	});
