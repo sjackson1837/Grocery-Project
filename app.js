315e8fca43fb8e90db524d52594e8972c33259c2
sjackson1837 <sjackson330@gmail.com>
2023-03-18 21:53:32 -0500

    init

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 function getProductInfo() {
 	const barcode = $('#barcode').val();
 	const url = `https://world.openfoodfacts.org/api/v0/product/${barcode}.json`;
-
+  
 	$.getJSON(url, function(data) {
+	  if (data.product) {
 		const product = data.product;
 		$('#product-name').text(product.product_name);
 		$('#product-description').text(product.generic_name);
 		$('#product-image').attr('src', product.image_url);
+	  } else {
+		$('#product-name').text('no product found');
+	  }
 	});
-}
+  }
+  
