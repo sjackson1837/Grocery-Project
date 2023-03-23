8a1715e036a4f28c46c0f241d7d51b386bc37509
sjackson1837 <sjackson330@gmail.com>
2023-03-23 16:55:00 -0500

    init

--- scripts/scripts-good.js
+++ scripts/scripts-good.js
@@ -6,32 +6,31 @@ function lookupProduct() {
       .then(response => response.json())
       .then(data => {
         const ProductName = data.product.product_name;
-        //const ProductDescription = data.product.generic_name;
+        const ProductDescription = data.product.generic_name;
         //const ProductQty = "new";
         var ProductQty = 1;
-        const imageUrl = data.product.image_url;
+        //const imageUrl = data.product.image_url;
 
         document.getElementById("ProductBarcode").value = barcode;
         document.getElementById("ProductName").value = ProductName;
-        //document.getElementById("ProductDescription").value = ProductDescription;
+        document.getElementById("ProductDescription").value = ProductDescription;
         document.getElementById("ProductQty").value = ProductQty;
-        document.getElementById("product-image").src = imageUrl;
+        //document.getElementById("product-image").src = imageUrl;
         document.getElementById("barcode").value = "";
-//        var audio = new Audio('Sounds/positive.mp3');
-//		    audio.play();
+        var audio = new Audio('Sounds/positive.mp3');
+		    audio.play();
         sendtogoogle();
       })
       .catch(error => console.error(error));
-//      var audio = new Audio('Sounds/negative.mp3');
-//      audio.play();
+      var audio = new Audio('Sounds/negative.mp3');
+      audio.play();
     
   }
 
   function sendtogoogle() {
   const scriptURL = 'https://script.google.com/macros/s/AKfycbzAew4P3qGozmSxEP22oLoEtyn_dJ-h2aiQpGy4rUzY7X5Tm-F_b5_pgfQQzZIxwF0_/exec'
   const form = document.forms['submit-to-google-sheet']
-        var audio = new Audio('Sounds/positive.mp3');
-		    audio.play();
+
 
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
       .then(response => console.log('Success!', response))
