1a8cb0839b8246d1acfe5ac86a745788e0dcc936
sjackson1837 <sjackson330@gmail.com>
2023-03-24 15:36:39 -0500

    init

--- scripts/qty.js
+++ scripts/qty.js
@@ -1,47 +1,41 @@
-// Load the Google Sheets API client library
-gapi.load('client', function() {
-    // Authenticate the client with the Google Sheets API using OAuth 2.0
-    // ...
-  
-    // Retrieve the data from the "Jackson Inventory" sheet
-    gapi.client.sheets.spreadsheets.values.get({
-      spreadsheetId: 'your-spreadsheet-id',
-      range: 'A:C'
-    }).then(function(response) {
-      // Process the data to get the unique barcodes and calculate the quantity for each item
-      var data = response.result.values;
-      var items = {};
-      data.forEach(function(row) {
-        var barcode = row[0];
-        var name = row[1];
-        var qty = Number(row[2]);
-        if (items[barcode]) {
-          items[barcode].qty += qty;
-        } else {
-          items[barcode] = {
-            name: name,
-            qty: qty
-          };
-        }
-      });
-  
-      // Display the data on the screen
-      var table = document.createElement('table');
-      var header = table.createTHead();
-      var row = header.insertRow(0);
-      row.insertCell(0).innerHTML = 'Barcode';
-      row.insertCell(1).innerHTML = 'Product Name';
-      row.insertCell(2).innerHTML = 'Qty';
-      var body = table.createTBody();
-      Object.keys(items).forEach(function(barcode) {
-        row = body.insertRow();
-        row.insertCell(0).innerHTML = barcode;
-        row.insertCell(1).innerHTML = items[barcode].name;
-        row.insertCell(2).innerHTML = items[barcode].qty;
-      });
-      document.body.appendChild(table);
-    }, function(reason) {
-      console.error('Error: ' + reason.result.error.message);
-    });
-  });
-  
+let SHEET_ID = '1ls6zFkH8epBpvfHcycXia4_M0irRBmj9z00r7LOMW6E'
+let SHEET_TITLE = 'data';
+let SHEET_RANGE = 'A:C'
+
+let FULL_URL = ('https://docs.google.com/spreadsheets/d/' + SHEET_ID + '/gviz/tq?sheet=' + SHEET_TITLE + '&range=' + SHEET_RANGE);
+
+fetch(FULL_URL)
+.then(res=>res.text())
+.then(rep=> {
+  let data = JSON.parse(rep.substr(47).slice(0, -2));
+ // console.log(data)
+
+ let barcode = document.getElementById('barcode');
+ let product = document.getElementById('product');
+ let srjbarcode = document.getElementById('srjbarcode');
+ let srjproduct = document.getElementById('srjproduct');
+ let length = data.table.rows.length -1;
+
+ console.log(data.table.rows[0].c[0].v);
+ barcode.innerHTML = data.table.rows[0].c[0].v;
+ product.innerHTML = data.table.rows[0].c[1].v;
+
+ for(let i = 0; i < length; i++) {
+   
+   let NewBoxBarcode = document.createElement('div');
+    NewBoxBarcode.id = ("box"+i);
+    NewBoxBarcode.className = "Some_Style";
+    barcode.append(NewBoxBarcode);
+    NewBoxBarcode.innerHTML = data.table.rows[i].c[0].v;
+
+    let NewBoxProduct = document.createElement('div');
+    NewBoxProduct.id = ("box"+i);
+    NewBoxProduct.className = "Some_Style";
+    barcode.append(NewBoxProduct);
+    NewBoxProduct.innerHTML = data.table.rows[i].c[1].v;
+
+    
+    
+ }
+
+})
