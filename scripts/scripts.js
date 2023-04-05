7b99cc512a0a88e2cbd44869544b69f5d8e8df81
sjackson1837 <sjackson330@gmail.com>
2023-04-05 16:53:55 -0700

    init

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,63 +1,40 @@
 function lookupProduct() {
-    const barcode = document.getElementById("barcode").value;
-    const url = `https://world.openfoodfacts.org/api/v0/product/${barcode}.json`;
+  const barcode = document.getElementById("barcode").value;
+  const url = `https://world.openfoodfacts.org/api/v0/product/${barcode}.json`;
 
-
-    fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error("Product not found");
-      }
-      return response.json();
-    })
+  fetch(url)
+    .then(response => response.json())
     .then(data => {
       const ProductName = data.product.product_name;
+      //const ProductDescription = data.product.generic_name;
+      //const ProductQty = "new";
       var ProductQty = 1;
       const imageUrl = data.product.image_url;
-  
+
       document.getElementById("ProductBarcode").value = barcode;
       document.getElementById("ProductName").value = ProductName;
+      //document.getElementById("ProductDescription").value = ProductDescription;
       document.getElementById("ProductQty").value = ProductQty;
       document.getElementById("product-image").src = imageUrl;
       document.getElementById("barcode").value = "";
+//        var audio = new Audio('Sounds/positive.mp3');
+//		    audio.play();
       sendtogoogle();
     })
-    .catch(error => {
-      console.error(error);
-      alert("Product not found");
-      var audio = new Audio('Sounds/negative.mp3');
-      audio.play();
-    })};
-
+    .catch(error => console.error(error));
+//      var audio = new Audio('Sounds/negative.mp3');
+//      audio.play();
   
-  //   fetch(url)
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       const ProductName = data.product.product_name;
-  //       var ProductQty = 1;
-  //       const imageUrl = data.product.image_url;
-
-  //       document.getElementById("ProductBarcode").value = barcode;
-  //       document.getElementById("ProductName").value = ProductName;
-  //       document.getElementById("ProductQty").value = ProductQty;
-  //       document.getElementById("product-image").src = imageUrl;
-  //       document.getElementById("barcode").value = "";
-  //       sendtogoogle();
-  //     })
-  //     .catch(error => console.error(error));
-  //     var audio = new Audio('Sounds/negative.mp3');
-  //     audio.play();
-    
-  // }
+}
 
 
-  function sendtogoogle() {
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbzAew4P3qGozmSxEP22oLoEtyn_dJ-h2aiQpGy4rUzY7X5Tm-F_b5_pgfQQzZIxwF0_/exec'
-  const form = document.forms['submit-to-google-sheet']
-        var audio = new Audio('Sounds/positive.mp3');
-		    audio.play();
+function sendtogoogle() {
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzAew4P3qGozmSxEP22oLoEtyn_dJ-h2aiQpGy4rUzY7X5Tm-F_b5_pgfQQzZIxwF0_/exec'
+const form = document.forms['submit-to-google-sheet']
+      var audio = new Audio('Sounds/positive.mp3');
+      audio.play();
 
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
-      .catch(error => console.error('Error!', error.message))
-  }
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+}
