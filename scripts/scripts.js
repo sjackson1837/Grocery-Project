853cab9562838330f0f4b9327a5dd835a58392b9
sjackson1837 <sjackson330@gmail.com>
2023-04-05 17:27:59 -0700

    init

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,15 +3,10 @@ function lookupProduct() {
   const url = `https://world.openfoodfacts.org/api/v0/product/${barcode}.json`;
 
   fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error("Product not found");
-      }
-      return response.json();
-    })
+    .then(response => response.json())
     .then(data => {
       const ProductName = data.product.product_name;
-      var ProductQty = 1;
+      const ProductQty = 1;
       const imageUrl = data.product.image_url;
 
       document.getElementById("ProductBarcode").value = barcode;
@@ -19,23 +14,26 @@ function lookupProduct() {
       document.getElementById("ProductQty").value = ProductQty;
       document.getElementById("product-image").src = imageUrl;
       document.getElementById("barcode").value = "";
-      sendtogoogle();
+      sendToGoogle();
     })
     .catch(error => {
       console.error(error);
-      alert("Product not found");
-      var audio = new Audio('Sounds/negative.mp3');
-      audio.play();
+      document.getElementById("ProductBarcode").value = barcode;
+      document.getElementById("ProductName").value = "";
+      document.getElementById("ProductQty").value = "";
+      document.getElementById("product-image").src = "";
+      document.getElementById("barcode").value = "";
+      document.getElementById("product-description").style.display = "block";
     });
 }
 
-function sendtogoogle() {
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbzAew4P3qGozmSxEP22oLoEtyn_dJ-h2aiQpGy4rUzY7X5Tm-F_b5_pgfQQzZIxwF0_/exec';
+function sendToGoogle() {
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbzAew4P3qGozmSxEP22oLoEtyn_dJ-h2aiQpGy4rUzY7X5Tm-F_b5_pgfQQzZIxwF0_/exec'
   const form = document.forms['submit-to-google-sheet'];
   var audio = new Audio('Sounds/positive.mp3');
   audio.play();
 
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(response => console.log('Success!', response))
-    .catch(error => console.error('Error!', error.message))
+    .catch(error => console.error('Error!', error.message));
 }
